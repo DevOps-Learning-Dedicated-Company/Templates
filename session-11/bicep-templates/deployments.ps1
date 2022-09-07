9dd8bfb8692115a21f8d7bc30bafe1bbc095e198
Parteg132 <98527844+Parteg132@users.noreply.github.com>
2022-09-07 15:22:45 +0200

    Update deployments.ps1

--- session-11/bicep-templates/deployments.ps1
+++ session-11/bicep-templates/deployments.ps1
@@ -2,22 +2,10 @@ $RG = "testingARM"
 
 #New-AzResourceGroup -Name $RG -Location "ukwest"
 
-New-AzResourceGroupDeployment `
-     -Name "sql" `
-     -ResourceGroupName $RG `
-     -TemplateFile 'sql.json' `
-     -serverName "sqlservertest2137" `
-     -region 'ukwest' `
-     -dbName 'sqldb2137' `
-     -dbPricing 'Standard' `
-     -dbBackupPolicy 'Local' `
-     -firewallRules ("0.0.0.0", "0.0.0.0") `
-     -Verbose
-
-# New-AzResourceGroupDeployment `
-#     -Name "sqlbicep" `
+#New-AzResourceGroupDeployment `
+#     -Name "sql" `
 #     -ResourceGroupName $RG `
-#     -TemplateFile 'sql.bicep' `
+#     -TemplateFile 'sql.json' `
 #     -serverName "sqlservertest2137" `
 #     -region 'ukwest' `
 #     -dbName 'sqldb2137' `
@@ -27,75 +15,87 @@ New-AzResourceGroupDeployment `
 #     -Verbose
 
 New-AzResourceGroupDeployment `
-     -Name "appservice" `
-     -ResourceGroupName $RG `
-     -TemplateFile 'appService.json' `
-     -serviceName 'appservice420' `
-     -appName 'linuxbasedapp420' `
-     -region 'ukwest' `
-     -pricingTier 'S1' `
-     -nodes '1' `
-     -dockerImage 'hompus.azurecr.io/samples/nginx:latest' `
-     -Verbose
+    -Name "sqlbicep" `
+    -ResourceGroupName $RG `
+    -TemplateFile 'sql.bicep' `
+    -serverName "sqlservertest2137" `
+    -region 'ukwest' `
+    -dbName 'sqldb2137' `
+    -dbPricing 'Standard' `
+    -dbBackupPolicy 'Local' `
+    -firewallRules ("0.0.0.0", "0.0.0.0") `
+    -Verbose
 
-# New-AzResourceGroupDeployment `
-#     -Name "appservicebicep" `
+#New-AzResourceGroupDeployment `
+#     -Name "appservice" `
 #     -ResourceGroupName $RG `
-#     -TemplateFile 'appService.bicep' `
-#     -serviceName 'appservicebicep420' `
-#     -appName 'linuxbasedappbicep420' `
+#     -TemplateFile 'appService.json' `
+#     -serviceName 'appservice420' `
+#     -appName 'linuxbasedapp420' `
 #     -region 'ukwest' `
 #     -pricingTier 'S1' `
-#     -nodes 1 `
+#     -nodes '1' `
 #     -dockerImage 'hompus.azurecr.io/samples/nginx:latest' `
+#    -Verbose
+
+New-AzResourceGroupDeployment `
+    -Name "appservicebicep" `
+    -ResourceGroupName $RG `
+    -TemplateFile 'appService.bicep' `
+    -serviceName 'appservicebicep420' `
+    -appName 'linuxbasedappbicep420' `
+    -region 'ukwest' `
+    -pricingTier 'S1' `
+    -nodes 1 `
+    -dockerImage 'hompus.azurecr.io/samples/nginx:latest' `
+    -Verbose
+
+#New-AzResourceGroupDeployment `
+#     -Name "storageacc" `
+#     -ResourceGroupName $RG `
+#     -TemplateFile 'storageAccount.json' `
+#     -storageName 'storageaccount420' `
+#     -region 'ukwest' `
+#     -skuName 'Standard_LRS' `
+#     -skuTier 'Standard' `
 #     -Verbose
 
 New-AzResourceGroupDeployment `
-     -Name "storageacc" `
+     -Name "storageAccBicep" `
      -ResourceGroupName $RG `
-     -TemplateFile 'storageAccount.json' `
-     -storageName 'storageaccount420' `
+     -TemplateFile '.\storageAccount.bicep' `
+     -storageName 'storageaccbicep6969' `
      -region 'ukwest' `
      -skuName 'Standard_LRS' `
-     -skuTier 'Standard' `
      -Verbose
+  
+#New-AzResourceGroupDeployment `
+#    -Name "backup" `
+#    -ResourceGroupName $RG `
+#    -TemplateFile 'backup.json' `
+#    -storageName 'storageaccount420' `
+#    -appName 'linuxbasedapp420' `
+#    -scheduleInterval '1' `
+#    -scheduleUnit 'day' `
+#    -retention '10' `
+#    -region 'ukwest' `
+#    -serviceName 'appservice420' `
+#    -pricingTier 'S1' `
+#    -skuName 'Standard_LRS' `
+#    -skuTier 'Standard' `
+#    -Verbose
 
-# New-AzResourceGroupDeployment `
-# -Name "storageAccBicep" `
-# -ResourceGroupName $RG `
-# -TemplateFile '.\storageAccount.bicep' `
-# -storageName 'storageaccbicep6969' `
-# -region 'ukwest' `
-# -skuName 'Standard_LRS' `
-# -Verbose
-    
 New-AzResourceGroupDeployment `
-    -Name "backup" `
-    -ResourceGroupName $RG `
-    -TemplateFile 'backup.json' `
-    -storageName 'storageaccount420' `
-    -appName 'linuxbasedapp420' `
-    -scheduleInterval '1' `
-    -scheduleUnit 'day' `
-    -retention '10' `
-    -region 'ukwest' `
-    -serviceName 'appservice420' `
-    -pricingTier 'S1' `
-    -skuName 'Standard_LRS' `
-    -skuTier 'Standard' `
-    -Verbose
-
-# New-AzResourceGroupDeployment `
-# -Name "backupbicep" `
-# -ResourceGroupName $RG `
-# -TemplateFile 'backup.bicep' `
-# -storageName 'storageaccbicep6969' `
-# -appName 'linuxbasedappbicep420' `
-# -frequencyInterval 1 `
-# -frequencyUnit 'day' `
-# -retention 10 `
-# -dbType 'SqlAzure' `
-# -dbName 'sqldb2137' `
-# -serverName 'sqlservertest2137' `
-# -serverPass 'Thisismysqlpassword1' `
-# -Verbose
+     -Name "backupbicep" `
+     -ResourceGroupName $RG `
+     -TemplateFile 'backup.bicep' `
+     -storageName 'storageaccbicep6969' `
+     -appName 'linuxbasedappbicep420' `
+     -frequencyInterval 1 `
+     -frequencyUnit 'day' `
+     -retention 10 `
+     -dbType 'SqlAzure' `
+     -dbName 'sqldb2137' `
+     -serverName 'sqlservertest2137' `
+     -serverPass 'Thisismysqlpassword1' `
+     -Verbose
