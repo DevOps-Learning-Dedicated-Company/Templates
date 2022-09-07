286e452c468e82f662748247c99d95f03a4e5fed
Parteg132 <98527844+Parteg132@users.noreply.github.com>
2022-09-07 15:18:55 +0200

    Update deployments.ps1

--- session-11/ARM-templates/deployments.ps1
+++ session-11/ARM-templates/deployments.ps1
@@ -2,17 +2,17 @@ $RG = "testingARM"
 
 #New-AzResourceGroup -Name $RG -Location "ukwest"
 
-# New-AzResourceGroupDeployment `
-#     -Name "sql" `
-#     -ResourceGroupName $RG `
-#     -TemplateFile 'sql.json' `
-#     -serverName "sqlservertest2137" `
-#     -region 'ukwest' `
-#     -dbName 'sqldb2137' `
-#     -dbPricing 'Standard' `
-#     -dbBackupPolicy 'Local' `
-#     -firewallRules ("0.0.0.0", "0.0.0.0") `
-#     -Verbose
+New-AzResourceGroupDeployment `
+    -Name "sql" `
+    -ResourceGroupName $RG `
+    -TemplateFile 'sql.json' `
+    -serverName "sqlservertest2137" `
+    -region 'ukwest' `
+    -dbName 'sqldb2137' `
+    -dbPricing 'Standard' `
+    -dbBackupPolicy 'Local' `
+    -firewallRules ("0.0.0.0", "0.0.0.0") `
+    -Verbose
 
 # New-AzResourceGroupDeployment `
 #     -Name "sqlbicep" `
@@ -26,17 +26,17 @@ $RG = "testingARM"
 #     -firewallRules ("0.0.0.0", "0.0.0.0") `
 #     -Verbose
 
-# New-AzResourceGroupDeployment `
-#     -Name "appservice" `
-#     -ResourceGroupName $RG `
-#     -TemplateFile 'appService.json' `
-#     -serviceName 'appservice420' `
-#     -appName 'linuxbasedapp420' `
-#     -region 'ukwest' `
-#     -pricingTier 'S1' `
-#     -nodes '1' `
-#     -dockerImage 'hompus.azurecr.io/samples/nginx:latest' `
-#     -Verbose
+New-AzResourceGroupDeployment `
+    -Name "appservice" `
+    -ResourceGroupName $RG `
+    -TemplateFile 'appService.json' `
+    -serviceName 'appservice420' `
+    -appName 'linuxbasedapp420' `
+    -region 'ukwest' `
+    -pricingTier 'S1' `
+    -nodes '1' `
+    -dockerImage 'hompus.azurecr.io/samples/nginx:latest' `
+    -Verbose
 
 # New-AzResourceGroupDeployment `
 #     -Name "appservicebicep" `
@@ -50,15 +50,15 @@ $RG = "testingARM"
 #     -dockerImage 'hompus.azurecr.io/samples/nginx:latest' `
 #     -Verbose
 
-# New-AzResourceGroupDeployment `
-#     -Name "storageacc" `
-#     -ResourceGroupName $RG `
-#     -TemplateFile 'storageAccount.json' `
-#     -storageName 'storageaccount420' `
-#     -region 'ukwest' `
-#     -skuName 'Standard_LRS' `
-#     -skuTier 'Standard' `
-#     -Verbose
+New-AzResourceGroupDeployment `
+    -Name "storageacc" `
+    -ResourceGroupName $RG `
+    -TemplateFile 'storageAccount.json' `
+    -storageName 'storageaccount420' `
+    -region 'ukwest' `
+    -skuName 'Standard_LRS' `
+    -skuTier 'Standard' `
+    -Verbose
 
 # New-AzResourceGroupDeployment `
 # -Name "storageAccBicep" `
@@ -70,20 +70,20 @@ $RG = "testingARM"
 # -Verbose
     
 New-AzResourceGroupDeployment `
--Name "backup" `
--ResourceGroupName $RG `
--TemplateFile 'backup.json' `
--storageName 'storageaccount420' `
--appName 'linuxbasedapp420' `
--scheduleInterval '1' `
--scheduleUnit 'day' `
--retention '10' `
--region 'ukwest' `
--serviceName 'appservice420' `
--pricingTier 'S1' `
--skuName 'Standard_LRS' `
--skuTier 'Standard' `
--Verbose
+    -Name "backup" `
+    -ResourceGroupName $RG `
+    -TemplateFile 'backup.json' `
+    -storageName 'storageaccount420' `
+    -appName 'linuxbasedapp420' `
+    -scheduleInterval '1' `
+    -scheduleUnit 'day' `
+    -retention '10' `
+    -region 'ukwest' `
+    -serviceName 'appservice420' `
+    -pricingTier 'S1' `
+    -skuName 'Standard_LRS' `
+    -skuTier 'Standard' `
+    -Verbose
 
 # New-AzResourceGroupDeployment `
 # -Name "backupbicep" `
