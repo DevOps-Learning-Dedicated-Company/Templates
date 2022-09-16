3f578b1d032b43aea77ecceb50ef61321a7a92ea
Parteg132 <98527844+Parteg132@users.noreply.github.com>
2022-09-16 18:06:35 +0200

    Delete script.sh

--- session-16/script.sh
+++
@@ -1,7 +0,0 @@
-az storage blob service-properties update \
-   --account-name "mbidzinskistorageaccount" \
-   --static-website \
-   --404-document "error.html" \
-   --index-document "index.html" \
-   --verbose \
-   -o table
