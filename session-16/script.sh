fb410d37090433fe7887f8454cdcf1bbd20a885f
Parteg132 <98527844+Parteg132@users.noreply.github.com>
2022-09-16 18:22:00 +0200

    Add files via upload

---
+++ session-16/script.sh
@@ -0,0 +1,7 @@
+az storage blob service-properties update \
+   --account-name "armContainerforStaticWeb" \
+   --static-website \
+   --404-document "404.html" \
+   --index-document "index.html" \
+   --verbose \
+   -o table
