0f42e0e9a590b0d41c99bc0657b894537694d093
Parteg132 <98527844+Parteg132@users.noreply.github.com>
2022-09-16 18:28:33 +0200

    Update script.sh

--- session-16/script.sh
+++ session-16/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 az storage blob service-properties update \
-   --account-name "armContainerforStaticWeb" \
+   --account-name "armconforstaticweb" \
    --static-website \
    --404-document "404.html" \
    --index-document "index.html" \
